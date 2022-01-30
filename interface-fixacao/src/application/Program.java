317de1976cbf401f723e59829862a4dd215ead34
cassemanuel <cassioefs@dcc.ufrj.br>
2022-01-30 18:22:56 -0300

    Exercicio Completo

--- interface-fixacao/src/application/Program.java
+++ interface-fixacao/src/application/Program.java
@@ -1,16 +1,49 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Contract;
+import entities.Installment;
 import services.ContractService;
 import services.OnlinePaymentService;
 import services.PaypalService;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
-		OnlinePaymentService ps = new PaypalService();
-		ContractService cs = new ContractService(ps);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter contract data");
+		System.out.print("Number: ");
+		Integer number = sc.nextInt();
+		System.out.print("Date (dd/MM/yyyy): ");
+		Date date = sdf.parse(sc.next());
+		System.out.print("Contract value: ");
+		Double totalValue = sc.nextDouble();
+
+		Contract contract = new Contract(number, date, totalValue);
+
+		System.out.print("Enter number of installments: ");
+		int N = sc.nextInt();
+
+		ContractService cs = new ContractService(new PaypalService());
+
+		cs.ProcessContract(contract, N);
+
+		System.out.println("Installments:");
+		for (Installment it : contract.getInstallments()) {
+			System.out.println(it);
+		}
+
+		sc.close();
 	}
 
 }
--- interface-fixacao/src/services/ContractService.java
+++ interface-fixacao/src/services/ContractService.java
@@ -1,6 +1,10 @@
 package services;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import entities.Contract;
+import entities.Installment;
 
 public class ContractService {
 
@@ -12,9 +16,22 @@ public class ContractService {
 
 	public void ProcessContract(Contract contract, int months) {
 		double basicQuota = contract.getTotalValue() / months;
+		// valor inicial das parcelas
 		for (int i = 1; i <= months; ++i) {
 			double updatedQuota = basicQuota + onlinePaymentService.interest(basicQuota, i);
+			// valor das parcelas + taxa básica mensal
+			double fullQuota = updatedQuota + onlinePaymentService.paymentFee(updatedQuota);
+			// valor da parcela + taxa fixa mensal
+			Date dueDate = addMonths(contract.getDate(), i);
+			contract.getInstallments().add(new Installment(dueDate, fullQuota));
 		}
 	}
-	
+
+	private Date addMonths(Date date, int N) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		calendar.add(Calendar.MONTH, N);
+		return calendar.getTime();
+	}
+
 }
