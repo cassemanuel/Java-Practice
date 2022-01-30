8e04752f6b2eb8c47def8dcbcf7b83c453c83bf1
cassemanuel <cassioefs@dcc.ufrj.br>
2022-01-30 16:24:12 -0300

    com interface

--- inteface-locadora/src/model/services/RentalService.java
+++ inteface-locadora/src/model/services/RentalService.java
@@ -4,12 +4,13 @@ import model.entities.CarRental;
 import model.entities.Invoice;
 
 public class RentalService {
+
 	private Double pricePerDay;
 	private Double pricePerHour;
 
-	private BrazilTaxService taxService;
+	private TaxService taxService;
 
-	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+	public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
 		this.pricePerDay = pricePerDay;
 		this.pricePerHour = pricePerHour;
 		this.taxService = taxService;
@@ -19,17 +20,16 @@ public class RentalService {
 		long t1 = carRental.getStart().getTime();
 		long t2 = carRental.getFinish().getTime();
 		double hours = (double) (t2 - t1) / 1000 / 60 / 60;
-		double basicPayment;
 
+		double basicPayment;
 		if (hours <= 12.0) {
-			basicPayment = Math.ceil(hours) * pricePerHour;
+			basicPayment = pricePerHour * Math.ceil(hours);
 		} else {
-			basicPayment = Math.ceil(hours / 24) * pricePerDay;
+			basicPayment = pricePerDay * Math.ceil(hours / 24);
 		}
-		
+
 		double tax = taxService.tax(basicPayment);
-		
-		carRental.setInvoice(new Invoice(basicPayment, tax));		
+
+		carRental.setInvoice(new Invoice(basicPayment, tax));
 	}
-	
 }
